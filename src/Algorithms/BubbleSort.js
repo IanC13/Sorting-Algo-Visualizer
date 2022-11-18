9a25a31f17b16a5694f0a40e91b3dc4e1d18139b
Ian <ian.yl.cheng@gmail.com>
2022-11-18 17:31:33 +0000

    Add visualization animation for BubbleSort
    Animation includes highlighting bars under comparison, highlighting the
    larger bar, swapping if necessary.

--- src/Algorithms/BubbleSort.js
+++ src/Algorithms/BubbleSort.js
@@ -1,21 +1,39 @@
-
-
-function bubbleSort(array, setCurrentBars) {
+function bubbleSort(array) {
   const length = array.length;
   let newArray = array.slice();
+
+  // animations array is a 2d array => [[0,1], [1,2], [2,3]...]
+  // holds the index of bars that are compared.
+  // length of this = total comparisons
+  let animations = [];
+
+  // Every state of the array after each pass
+  let allArrayStates = [];
+
+  // holds index of the larger element for each comparison
+  let largerArray = [];
 
   for (let i = 0; i < length; i++) {
     let swap = false;
 
     for (let j = 0; j < length - 1 - i; j++) {
       if (newArray[j] > newArray[j+1]) {
+        largerArray.push([j, j+1]);
+
         let temp = newArray[j];
         newArray[j] = newArray[j+1];
 
         newArray[j+1] = temp;
+        
 
         swap = true;
-      }        
+      } else {
+        largerArray.push([j+1, j+1]);
+      }
+
+
+      animations.push([j, j+1]);
+      allArrayStates.push([...newArray]);
     }
 
     if (swap === false) {
@@ -23,7 +41,7 @@ function bubbleSort(array, setCurrentBars) {
     }
   }
 
-  return newArray;
+  return { allArrayStates, animations, largerArray };
 }
 
 
--- src/BarVisualizer/Body/BarVisualizerBody.js
+++ src/BarVisualizer/Body/BarVisualizerBody.js
@@ -10,18 +10,28 @@ import bubbleSort from '../../Algorithms/BubbleSort';
 
 const NUMBER_OF_ITEMS = 100;
 const NUMBER_OF_SPACES = NUMBER_OF_ITEMS - 1;
-let DEFAULT_COLOR = '#EA4C89';
-let HIGHLIGHT_COLOR = 'red';
+const ANIMATION_DELAY_MS = 10;
+const ANIMATION_COLOR_DELAY_MS = 250;
+
+const DEFAULT_COLOR = '#EA4C89';
+const HIGHLIGHT_COLOR = 'turquoise';
+const LARGER_COLOR = 'green'; // Color for the larger element when comparing
 
 // Function component using React Hooks 
 function BarVisualizerBody() {
   // States
-  const [array, setArray] = useState([4,2,9,4,7,1]);
+  // The array to be sorted
+  const [array, setArray] = useState([]);
+  
+  // Keeps track of the 2 bars we are currently comparing
   const [currentBars, setCurrentBars] = useState([]);
+
+  // Larger of the comparisons
+  const [largerBar, setLarger] = useState();
 
   // componentDidMount eqv only 
   useEffect(function () {
-    //newArray();
+    newArray();
   }, [])
 
   function newArray() {
@@ -35,12 +45,62 @@ function BarVisualizerBody() {
     // Set array state
     setArray(tempArray);
     setCurrentBars([]);
+    setLarger([]);
   }
 
   // Make an animations array, a 2 d array which holds all the comparison
 
   function bubbleSortFunction() {
-    bubbleSort(array, setCurrentBars);
+    let {allArrayStates, animations, largerArray} = bubbleSort(array);
+
+    const length = animations.length;
+
+    // Delay from 2 same color highlighted bar to larger one highlighted with a different color
+    let largerColorDelay = ANIMATION_COLOR_DELAY_MS;
+    // Delay from 2 differently highlighted bar to swapping
+    let swapDelay = largerColorDelay + ANIMATION_COLOR_DELAY_MS;
+
+    // let largerColorDelay = 1000;
+    // // Delay from 2 differently highlighted bar to swapping
+    // let swapDelay = largerColorDelay + 10;
+
+    for (let i = 0; i < length; i++) {
+      setTimeout(() => {
+        // Current 2 bars we are comparing. state change so re renders which trigger 
+        // the color change
+        setCurrentBars(animations[i]);
+
+        // highlight the 2 comparing bars for some time before coloring the larger one 
+        setTimeout(() => {
+          // larger of the two, re render with a different color
+          setLarger(largerArray[i][0]);
+        }, largerColorDelay);
+
+        // wait then swap
+        setTimeout(() => {
+          // Triggers re render, display swapped position
+          setArray(allArrayStates[i]);
+          setLarger(largerArray[i][1]);
+        }, swapDelay);
+
+        // reset swapped bars to be same color
+        setLarger();
+      }, i * (ANIMATION_DELAY_MS + (largerColorDelay + swapDelay)));
+      /*
+      i * DELAY because setTimeout is non-blocking i.e. entire for loop will 
+      run during wait time. for loop runs pretty much instantly so everything
+      in setTimeout only runs once as DELAY > for loop
+
+      i * DELAY solves this by creating a timer every pass as using 'let', 
+      a unique i is declared for each iteration and the timer 
+      is old time + DELAY (as i is 1 bigger than i-1)
+
+      USE WITH CAUTION. A new timer is created on every pass which means
+      length number of timers are created, a lot more memory usage.
+      */
+    }
+    setCurrentBars([]);
+    setLarger([]);
   }
 
   // render
@@ -61,7 +121,8 @@ function BarVisualizerBody() {
             // Making of the bars
             style={ { height: `${(value/Math.max(...array)) * 100}%`,
                       width: `${100/(NUMBER_OF_ITEMS + NUMBER_OF_SPACES)}%`,
-                      backgroundColor: (id === currentBars[0] || id === currentBars[1]) ? HIGHLIGHT_COLOR : DEFAULT_COLOR } }
+                      // currentBars hold the 2 bars that we are comparing. change colors accordingly
+                      backgroundColor: (id === largerBar ? LARGER_COLOR : (id === currentBars[0] || id === currentBars[1]) ? HIGHLIGHT_COLOR : DEFAULT_COLOR) } }
           >
           </div>
         )}
